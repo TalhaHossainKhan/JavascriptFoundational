9649bb843640af0bf6cc2c164c65c307bb450eac
TalhaHossainKhan <talhahossain75@gmail.com>
2024-07-17 22:54:55 -0400

    learnt loops and array

---
+++ array.js
@@ -0,0 +1,6 @@
+const numArray = [0,1,2,3,4,5,6,7,8]
+
+numArray.push(9)
+let popped = numArray.pop()
+
+console.log(numArray[numArray.length - 1])
---
+++ loop.js
@@ -0,0 +1,10 @@
+let flag = false
+
+while (flag == false){
+    console.log("I love women")
+    flag = true
+}
+
+for (let i = 0; i < 5; i++){
+    console.log(i)
+}
